b027538110d926d5967cbfedd57f15a59d8d5880
Farzeen <12694111+thefzsalam@users.noreply.github.com>
2018-04-03 21:15:47 +0530

    Fixed imports.

--- src/main/java/in/fzs/libredl/download_list_view/DownloadListViewData.java
+++ src/main/java/in/fzs/libredl/download_list_view/DownloadListViewData.java
@@ -1,6 +1,6 @@
 package in.fzs.libredl.download_list_view;
 
-import in.fzs.libredl.DownloadState;
+import in.fzs.libredl.common.DownloadState;
 
 public class DownloadListViewData {
     String name;
