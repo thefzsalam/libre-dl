e5d6795b0e800a8170b6bf1eba0b0d7f630d0746
Farzeen <12694111+thefzsalam@users.noreply.github.com>
2018-04-03 21:15:34 +0530

    Fixed imports, refactor to apply new name: IViewRequestsDispatcher -> IViewRequestsHandler

--- src/main/java/in/fzs/libredl/download_list_view/AbstractDownloadListView.java
+++ src/main/java/in/fzs/libredl/download_list_view/AbstractDownloadListView.java
@@ -1,12 +1,12 @@
 package in.fzs.libredl.download_list_view;
 
-import in.fzs.libredl.Entry;
+import in.fzs.libredl.common.Entry;
 
 public abstract class AbstractDownloadListView {
 
-    protected final IViewRequestsDispatcher view_request_dispatcher;
+    protected final IViewRequestHandler view_request_dispatcher;
 
-    public AbstractDownloadListView(IViewRequestsDispatcher view_request_dispatcher) {
+    public AbstractDownloadListView(IViewRequestHandler view_request_dispatcher) {
         this.view_request_dispatcher = view_request_dispatcher;
     }
 
