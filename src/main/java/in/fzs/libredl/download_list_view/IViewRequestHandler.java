4166ca64bf7e45f20b14a252e01aa48c8a786a19
Farzeen <12694111+thefzsalam@users.noreply.github.com>
2018-04-04 11:34:04 +0530

    Added request_pause method in IViewRequestHandler Interface. When the user clicks on pause button for a download entry, the implementation of AbstractDownloadListView calls this method to request the app to pause the corresponding download. If the pause succeeded, update_entry must be called on AbstractDownloadListView with the updated value of state in DownloadListViewData

--- src/main/java/in/fzs/libredl/download_list_view/IViewRequestHandler.java
+++ src/main/java/in/fzs/libredl/download_list_view/IViewRequestHandler.java
@@ -1,6 +1,7 @@
 package in.fzs.libredl.download_list_view;
 
 
+import in.fzs.libredl.app.DownloadListView;
 import in.fzs.libredl.common.Entry;
 
 public interface IViewRequestHandler {
@@ -8,5 +9,5 @@ public interface IViewRequestHandler {
     void request_add();
     void request_update(Entry<DownloadListViewData> entry_to_update);
     void request_delete(Entry<DownloadListViewData> entry_to_delete);
-
+    void request_pause(Entry<DownloadListViewData> entry_to_pause);
 }
