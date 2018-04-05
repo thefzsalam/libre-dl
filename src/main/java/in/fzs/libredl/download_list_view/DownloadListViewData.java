6fec62e37ae7bb95f6989d957dac323b7f8837df
Farzeen <12694111+thefzsalam@users.noreply.github.com>
2018-04-05 22:34:39 +0530

    Fixed access of members. was default access, made public.

--- src/main/java/in/fzs/libredl/download_list_view/DownloadListViewData.java
+++ src/main/java/in/fzs/libredl/download_list_view/DownloadListViewData.java
@@ -3,7 +3,7 @@ package in.fzs.libredl.download_list_view;
 import in.fzs.libredl.common.DownloadState;
 
 public class DownloadListViewData {
-    String name;
-    float progress;
-    DownloadState state;
+    public String name;
+    public float progress;
+    public DownloadState state;
 }
