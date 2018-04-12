9b84caf33306e990e5257da24c9c7e38ef6236b3
Farzeen <12694111+thefzsalam@users.noreply.github.com>
2018-04-12 22:45:50 +0530

    Added constructor for Map.Entry<Long,T> for convenience.

--- src/main/java/in/fzs/libredl/common/Entry.java
+++ src/main/java/in/fzs/libredl/common/Entry.java
@@ -1,5 +1,7 @@
 package in.fzs.libredl.common;
 
+import java.util.Map;
+
 public class Entry<T> {
     public final long id;
     public final T item;
@@ -7,5 +9,10 @@ public class Entry<T> {
     public Entry(long id, T item) {
         this.id = id;
         this.item = item;
+    }
+
+    public Entry(Map.Entry<Long,T> entry) {
+        this.id = entry.getKey();
+        this.item = entry.getValue();
     }
 }
