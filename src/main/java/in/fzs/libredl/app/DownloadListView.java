4eb61118e8a46df36082bcfdf3e1dc69a69b6dd0
Farzeen <12694111+thefzsalam@users.noreply.github.com>
2018-04-12 22:57:42 +0530

    Moved download_list_adapter from being a member of DownloadListView.Widgets class to DownloadListView itself.
    Widgets was supposed to be passed on to DownloadListView constructor by
    MainActivity.
    
    Currently, it is designed so that DownloadListView initializes
    DownloadListViewAdapter using its own Map<Long,DownloadListViewData> entries, and
    its supplied instance of IViewRequestsHandler.
    
    TODO: Figure out is it some way better or worse to pass DownloadListViewAdapter
    also as a constructor parameter.

--- src/main/java/in/fzs/libredl/app/DownloadListView.java
+++ src/main/java/in/fzs/libredl/app/DownloadListView.java
@@ -1,6 +1,7 @@
 package in.fzs.libredl.app;
 
 import android.view.View;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 import java.util.HashMap;
@@ -15,24 +16,27 @@ public class DownloadListView extends AbstractDownloadListView {
 
     private class Widgets {
         ListView download_list;
-        DownloadListAdapter download_list_adapter;
         View add_btn;
     }
 
     private final Widgets widgets;
     private final Map<Long,DownloadListViewData> download_list_entries;
+    private final BaseAdapter download_list_adapter;
+
     private long id_of_last_download_list_entry = 0;
+
 
     public DownloadListView(IViewRequestHandler view_request_handler, Widgets widgets) {
         super(view_request_handler);
         this.widgets = widgets;
         download_list_entries = new HashMap<>();
         initializeWidgets();
+        download_list_adapter = new DownloadListAdapter(download_list_entries, view_request_handler);
     }
 
     @Override
     public Entry<DownloadListViewData> add_to_view(DownloadListViewData data) {
-        id_of_last_download_list_entry+=1;
+        id_of_last_download_list_entry += 1;
         download_list_entries.put(id_of_last_download_list_entry, data);
         notifyListViewOfDataChange();
         return new Entry<>(id_of_last_download_list_entry, data);
@@ -51,22 +55,22 @@ public class DownloadListView extends AbstractDownloadListView {
     }
 
     private void initializeWidgets() {
-        widgets.download_list_adapter = new DownloadListAdapter(download_list_entries);
-        widgets.download_list.setAdapter(widgets.download_list_adapter);
+        widgets.download_list.setAdapter(download_list_adapter);
         widgets.add_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                view_request_handler.request_add();
+                view_request_handler.request_add_download();
             }
         });
         /** TODO:
          *  - Add delete and update button.
          *      In download_entry.xml or the action bar of list view.
-         *
+         *  TODO: More info
+         *  Tell DownloadListAdapter how to handle edit, delete, pause/start actions.
          */
     }
 
     private void notifyListViewOfDataChange() {
-        widgets.download_list_adapter.notifyDataSetChanged();
+        download_list_adapter.notifyDataSetChanged();
     }
 }
